8598612eea2a8045c23adaa16ab8696fa5cf1ac3
simplon <costanzo.isabel@gmail.com>
2017-07-05 14:11:59 +0200

    j' ai fait jusqu'au 6

--- array/app.js
+++ array/app.js
@@ -1,1 +1,15 @@
-var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+/*var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];*/
+var mois=["janvier", "fevrier","mars","avril","mai","juin","juillet", "aout", "septembre", "octobre", "novembre", "decembre"];
+
+console.log(mois[2]);
+
+console.log(mois[(6)]);
+
+console.log(mois[10]);
+
+mois[7]="août";
+console.log(mois[7]);
+
+
+var année=mois.join();
+console.log(année);
